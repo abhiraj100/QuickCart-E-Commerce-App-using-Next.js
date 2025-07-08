b89e0e082d88fa2311b6dd82d4c3f646e4a16532
ABHIRAJ YADAV <102871390+abhiraj100@users.noreply.github.com>
2025-07-08 08:27:07 +0530

    minor changes

--- app/layout.js
+++ app/layout.js
@@ -7,8 +7,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 const outfit = Outfit({ subsets: ["latin"], weight: ["300", "400", "500"] });
 
 export const metadata = {
-  title: "QuickCart - GreatStack",
-  description: "E-Commerce with Next.js ",
+  title: "QuickCart",
+  description: "Lightning-Fast E-Commerce with Next.js ",
 };
 
 export default function RootLayout({ children }) {
